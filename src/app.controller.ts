aeda0d1e25585661662f95a4262d8a4ad08ffbb2
MigsBroed <miguel.broedel09@gmail.com>
2025-07-18 13:50:29 -0300

    colors implement

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,8 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { Query, Res, HttpException, HttpStatus } from '@nestjs/common';
+import { Response } from 'express';
+import ColorThief from 'colorthief';
 
 @Controller()
 export class AppController {
@@ -9,4 +12,20 @@ export class AppController {
   getHello(): string {
     return this.appService.getHello();
   }
+
+  @Get('colors')
+  async getColors(@Query('url') url: string, @Res() res: Response) {
+    if (!url) {
+      throw new HttpException('Image URL is required', HttpStatus.BAD_REQUEST);
+    }
+
+    try {
+      const palette = await ColorThief.getPalette(url, 5); // 5 cores dominantes
+      res.json({ palette }); // formato: [[r,g,b], [r,g,b], ...]
+    } catch (err) {
+      throw new HttpException('Erro ao extrair cores', HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+  }
+
+
 }
