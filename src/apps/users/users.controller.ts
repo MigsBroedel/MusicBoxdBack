28ec29cc2c7d91a977a43094115d8d9c6eb4081e
MigsBroed <miguel.broedel09@gmail.com>
2025-07-28 13:20:25 -0300

    login feature

--- src/apps/users/users.controller.ts
+++ src/apps/users/users.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, Query } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
@@ -22,6 +22,23 @@ export class UsersController {
     return this.usersService.findBySpotifyID(id);
   }
 
+  @Post('logProcess/:spotifyId')
+  async findOrCreate(
+  @Param('spotifyID') spotifyID: string,
+  @Query('name') name?: string,
+) {
+  let user = await this.usersService.findBySpotifyID(spotifyID);
+
+  if (!user) {
+    user = await this.usersService.create({
+      name: name || 'Desconhecido',
+      spotifyID,
+      colors: '#fff'
+    });
+  }
+
+  return user;
+}
 
   @Get(':id')
   findOne(@Param('id') id: string) {
